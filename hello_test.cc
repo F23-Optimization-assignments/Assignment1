15a02281c9d529d54709d677cea386784edbdf63
Evgeny Bobkunov <jony.zh@yandex.ru>
2023-10-06 18:53:51 +0300

    feat(testing): add a real unit test

--- hello_test.cc
+++ hello_test.cc
@@ -1,9 +1,27 @@
 #include <gtest/gtest.h>
+#include <sstream>
+#include "src/simplex.h"
+#include "src/fraction.h"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-// Expect two strings not to be equal.
-EXPECT_STRNE("hello", "world");
-// Expect equality.
-EXPECT_EQ(7 * 6, 42);
+
+TEST(SimplexOptimumTest, Example1) {
+    // Define the input data for the Simplex function
+    std::vector<Fraction> coeffs = { -2, 3, -6, -1, 0, 0 };
+    Matrix<Fraction> m(3, 6);
+    std::istringstream input_matrix("2 1 -2 1 0 0\n1 2 4 0 1 0\n1 -1 2 0 0 1");
+    input_matrix >> m;
+    std::vector<Fraction> b = { 24, 22, 10 };
+    std::vector<size_t> basic_indices = { 3, 4, 5 };
+
+    // Create a Simplex object and find the solution
+    Simplex<Fraction> simplex(coeffs, m, b, basic_indices);
+    Solution<Fraction> solution = simplex.find_solution();
+
+    // Define the expected optimum value
+    int expected_optimum = -64;
+
+    // Check if the actual optimum matches the expected optimum
+    EXPECT_EQ(solution.is_final(), true);
+    EXPECT_EQ(solution.is_unbounded(), false);
+    EXPECT_EQ(solution.get_optimum(), expected_optimum);
 }
