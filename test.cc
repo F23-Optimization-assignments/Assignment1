7919fa2d4bc0e59f5168bce1a14f26e899c8d9a1
Artur <arturmuxutdinov@gmail.com>
2023-10-08 23:35:51 +0300

    feat: added tests for the incorrect inputs

--- test.cc
+++ test.cc
@@ -4,8 +4,42 @@
 #include "src/fraction.h"
 
 
+TEST(SimplexOptimumTest, IncorrectInput1) {
+    std::vector<Fraction> coeffs = {-2, 3, -6, -1, 0, 0};
+    Matrix<Fraction> m(3, 6);
+    std::istringstream input_matrix("2 1 -2 0 0 0\n"
+                                    "1 2 4 0 1 0\n"
+                                    "1 -1 2 0 0 1");
+    input_matrix >> m;
+    std::vector<Fraction> b = {24, 22, 10};
+    std::vector<size_t> basic_indices = {3, 4, 5};
+
+    Simplex<Fraction> simplex(coeffs, m, b, basic_indices);
+    Solution<Fraction> solution = simplex.find_solution();
+
+    EXPECT_EQ(solution.is_final(), false);
+    EXPECT_EQ(solution.is_unbounded(), false);
+}
+
+TEST(SimplexOptimumTest, IncorrectInput2) {
+    std::vector<Fraction> coeffs = {-2, 3, -6, -1, 0, 0};
+    Matrix<Fraction> m(3, 6);
+    std::istringstream input_matrix("2 1 -2 2 0 0\n"
+                                    "1 0 4 0 1 0\n"
+                                    "1 0 2 0 0 1");
+    input_matrix >> m;
+    std::vector<Fraction> b = {24, 22, 10};
+    std::vector<size_t> basic_indices = {3, 4, 5};
+
+    Simplex<Fraction> simplex(coeffs, m, b, basic_indices);
+    Solution<Fraction> solution = simplex.find_solution();
+
+    EXPECT_EQ(solution.is_final(), false);
+    EXPECT_EQ(solution.is_unbounded(), false);
+}
+
 TEST(SimplexOptimumTest, Example1) {
-    // Define the input data for the Simplex function
+// Define the input data for the Simplex function
     std::vector<Fraction> coeffs = {-2, 3, -6, -1, 0, 0};
     Matrix<Fraction> m(3, 6);
     std::istringstream input_matrix("2 1 -2 1 0 0\n1 2 4 0 1 0\n1 -1 2 0 0 1");
@@ -13,14 +47,14 @@ TEST(SimplexOptimumTest, Example1) {
     std::vector<Fraction> b = {24, 22, 10};
     std::vector<size_t> basic_indices = {3, 4, 5};
 
-    // Create a Simplex object and find the solution
+// Create a Simplex object and find the solution
     Simplex<Fraction> simplex(coeffs, m, b, basic_indices);
     Solution<Fraction> solution = simplex.find_solution();
 
-    // Define the expected optimum value
+// Define the expected optimum value
     int expected_optimum = -64;
 
-    // Check if the actual optimum matches the expected optimum
+// Check if the actual optimum matches the expected optimum
     EXPECT_EQ(solution.is_final(), true);
     EXPECT_EQ(solution.is_unbounded(), false);
     EXPECT_EQ(solution.get_optimum(), expected_optimum);
@@ -65,7 +99,9 @@ TEST(SimplexOptimumTest, Example3) {
 TEST(SimplexOptimumTest, Example4) {
     std::vector<Fraction> coeffs = {-2, -3, -4, 0, 0, 0};
     Matrix<Fraction> m(3, 6);
-    std::istringstream input_matrix("0 -2 -3 -1 0 0\n1 1 2 0 1 0\n1 2 3 0 0 1");
+    std::istringstream input_matrix("0 -2 -3 -1 0 0\n"
+                                    "1 1 2 0 1 0\n"
+                                    "1 2 3 0 0 1");
     input_matrix >> m;
     std::vector<Fraction> b = {-5, 4, 7};
     std::vector<size_t> basic_indices = {3, 4, 5};
@@ -73,11 +109,8 @@ TEST(SimplexOptimumTest, Example4) {
     Simplex<Fraction> simplex(coeffs, m, b, basic_indices);
     Solution<Fraction> solution = simplex.find_solution();
 
-    Fraction expected_optimum = Fraction(-21, 2);
-
-    EXPECT_EQ(solution.is_final(), true);
+    EXPECT_EQ(solution.is_final(), false);
     EXPECT_EQ(solution.is_unbounded(), false);
-    EXPECT_EQ(solution.get_optimum(), expected_optimum);
 }
 
 TEST(SimplexOptimumTest, Example5) {
